023931475de1e14ef3c6f49ef42e05f646ece65c
Giammarco <g.boscaro@im-tech.it>
2021-12-02 21:59:05 +0100

    disabled DB connection to run without ms drivers

--- src/DBManager.java
+++ src/DBManager.java
@@ -1,73 +1,76 @@
-import java.sql.*;
+// import java.sql.*;
 
-public class DBManager{
-	
-	String url= "jdbc:odbc:Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=Data.mdb;";
-	Connection conn;         
-	
-	public void addUrl(String str){
-		
-		url = "jdbc:odbc:Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ="+str+";";
-	}	
+// public class DBManager{
 
-	public ResultSet query(String query){
-		
-	    try{
-	    	
-	        Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
-	        ResultSet rs = stmt.executeQuery(query);
-	        return rs;
-	        
-	    }catch(SQLException e){
-	    	
-	    	System.out.println("Query Error: "+e.getMessage());
-	        closeConnection();
-	        return null;
-	    }
-	}
-	public void update(String update){
-		
-	    try{
-	    	
-	        Statement stmt = conn.createStatement();
-	        stmt.execute(update);
-	        
-	    }catch(SQLException e){
-	    	
-	    	System.out.println("Update Error: "+e.getMessage());
-	        closeConnection();
-	    }
-	}
-	
-	public void openConnection(){
-		
-	    try{
-	    	
-	        Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
-	        
-	    }catch(ClassNotFoundException e){
-	    	
-	    	System.out.println("Driver Error: "+e.getMessage());
-	    	
-	    }try{
-	    	
-	        conn = DriverManager.getConnection(url);
-	        
-	    }catch(SQLException e){
-	    	
-	    	System.out.println("Connection Error: "+e.getMessage());
-	    }
-	}
-	
-	public void closeConnection(){
-		
-	    try{
-	    	
-	        conn.close();
-	        
-	    }catch(SQLException e){
-	    	
-	    	System.out.println("Close Error: "+e.getMessage());
-	    }
-	}
-}
+// String url= "jdbc:odbc:Driver={Microsoft Access Driver (*.mdb,
+// *.accdb)};DBQ=Data.mdb;";
+// Connection conn;
+
+// public void addUrl(String str){
+
+// url = "jdbc:odbc:Driver={Microsoft Access Driver (*.mdb,
+// *.accdb)};DBQ="+str+";";
+// }
+
+// public ResultSet query(String query){
+
+// try{
+
+// Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
+// ResultSet.CONCUR_UPDATABLE);
+// ResultSet rs = stmt.executeQuery(query);
+// return rs;
+
+// }catch(SQLException e){
+
+// System.out.println("Query Error: "+e.getMessage());
+// closeConnection();
+// return null;
+// }
+// }
+// public void update(String update){
+
+// try{
+
+// Statement stmt = conn.createStatement();
+// stmt.execute(update);
+
+// }catch(SQLException e){
+
+// System.out.println("Update Error: "+e.getMessage());
+// closeConnection();
+// }
+// }
+
+// public void openConnection(){
+
+// try{
+
+// Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+
+// }catch(ClassNotFoundException e){
+
+// System.out.println("Driver Error: "+e.getMessage());
+
+// }try{
+
+// conn = DriverManager.getConnection(url);
+
+// }catch(SQLException e){
+
+// System.out.println("Connection Error: "+e.getMessage());
+// }
+// }
+
+// public void closeConnection(){
+
+// try{
+
+// conn.close();
+
+// }catch(SQLException e){
+
+// System.out.println("Close Error: "+e.getMessage());
+// }
+// }
+// }
